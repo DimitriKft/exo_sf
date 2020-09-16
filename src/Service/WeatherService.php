af5686e9d02c4c75682134b0fa102773d5262acf
klopfstein <dimitri.kft@gmail.com>
2020-09-16 17:07:33 +0200

    set up icon meteo

--- src/Service/WeatherService.php
+++ src/Service/WeatherService.php
@@ -24,44 +24,17 @@ class WeatherService
         $this->apiKey = $apiKey;
     }
 
-  
-
-   
-    // public function getApi(Request $request)
-    // {
-    //     $city_search    = $request->request->get('search_city', 'Toulouse');
-    //     $response       = $this->client->request('GET',         'https://api.openweathermap.org/data/2.5/weather?q=' . $city_search .',fr&appid=' . $this->apiKey . '&units=metric');
-    //     $status         = $response->getStatusCode();
-    //     // dd($status);
-    //     // die();
-    //     $content        = $response->getContent();
-    //     $content_decode = json_decode($content);
-        
-    //     return $content_decode;
-    // }
 
     public function getApi(Request $request)
     {
-      
-        // if (200 !== $response->getStatusCode()) {
-        //     throw new \Exception('blablbla');
-        // }
-
-        // catch(\Exception $e) 
-        // {
-        //     if (200 !== $response->getStatusCode()) {
-        //         throw new \Exception('<h1>Blabla</h1>');
-        //     }
-        // }
-
         try
         {
             $city_search    = $request->request->get('search_city', 'Toulouse');
-            $response       = $this->client->request('GET',         'https://api.openweathermap.org/data/2.5/weather?q=' . $city_search .',fr&appid=' . $this->apiKey . '&units=metric&lang=fr');
-            $contentType    = $response->getHeaders()['content-type'][0];
+            $response       = $this->client->request('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' . $city_search .'&appid=' . $this->apiKey . '&units=metric&lang=fr');
             $content        = $response->getContent();
             
-        }catch(\Exception $e) 
+        }
+        catch(\Exception $e) 
         {
             if (200 !== $response->getStatusCode()) 
             {
